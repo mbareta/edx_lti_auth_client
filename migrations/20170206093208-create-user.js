73f08843baad08015e76afa301e81cd3e0de80c5
Miro Dojkic <mdojkic@gmail.com>
2017-02-14 14:23:51 +0100

    Migrations to es6

--- migrations/20170206093208-create-user.js
+++ migrations/20170206093208-create-user.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('Users', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -19,9 +19,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    })
   },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
-  }
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Users')
 };
