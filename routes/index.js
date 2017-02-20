59b52703a864278b56118c4c16abb0300ac49830
Marin Bareta <mbareta@extensionengine.com>
2017-02-20 16:39:12 +0100

    Fix user flow

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Portfolio' });
+  console.log(req.session.user);
+  res.render('index', { title: 'Portfolio', email: req.session.user.email });
 });
 
 module.exports = router;
