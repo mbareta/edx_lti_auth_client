adf1150a5baef3ec34a47023de955f1519504258
Marin Bareta <mbareta@extensionengine.com>
2017-05-11 15:30:39 +0200

    Fetch disabled blocks

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const EdxCourseApi = require('../lib/edxCourseApi');
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', { title: 'Portfolio' });
+});
+
+router.get('/blocks', (req, res) => {
+  EdxCourseApi.getDisabledBlocks(req.session.user, req.session.token.access_token)
+  .then(results => res.send(results))
 });
 
 module.exports = router;
