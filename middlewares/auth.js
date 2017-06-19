31839a73e8e6d72994abf8beb9841ed72332b398
Marin Bareta <mbareta@extensionengine.com>
2017-06-19 12:24:28 +0200

    ES6-ize

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -30,8 +30,9 @@ const cacheUserXBlocks = (req, res, next) => {
 const setUserCookies = (req, res, next) => {
   const cookies = req.session.edxCookies;
   const encode = (cookie) => decodeURIComponent(cookie);
+  const domain = config.cookieDomain;
   cookies.forEach(cookie =>
-    res.cookie(cookie.split('=')[0], cookie.split('=')[1].split(';')[0], { encode, domain: config.cookieDomain })
+    res.cookie(cookie.split('=')[0], cookie.split('=')[1].split(';')[0], { encode, domain })
   );
   next();
 };
