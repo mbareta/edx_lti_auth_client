d5853c98060e9f832683b4962139398e7ce72f1e
Marin Bareta <mbareta@extensionengine.com>
2017-06-21 13:42:20 +0200

    Remove crap

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -27,18 +27,7 @@ const cacheUserXBlocks = (req, res, next) => {
   .then(next);
 };
 
-const setUserCookies = (req, res, next) => {
-  const cookies = req.session.edxCookies;
-  const encode = (cookie) => decodeURIComponent(cookie);
-  const domain = config.cookieDomain;
-  cookies.forEach(cookie =>
-    res.cookie(cookie.split('=')[0], cookie.split('=')[1].split(';')[0], { encode, domain })
-  );
-  next();
-};
-
 module.exports = {
   getUserInfo,
-  cacheUserXBlocks,
-  setUserCookies
+  cacheUserXBlocks
 };
