0452a9f7de066a9e0eb30954ac4ae7ab56e547c7
Miro Dojkic <mdojkic@gmail.com>
2017-07-17 10:24:36 +0200

    KF-355: Update favicon image (#116)
    * update favicon
    
    * use express instead of html

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ global.Promise = require('bluebird');
 
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon'); // eslint-disable-line
+const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -42,8 +42,7 @@ app.locals.config = config;
 // HTTPS proxy magic
 app.enable('trust proxy');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'assets', 'images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
