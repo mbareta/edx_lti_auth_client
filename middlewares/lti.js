78b5391dbbffbf4c2a687a25abb4444b5f839fc4
Miro Dojkic <mdojkic@gmail.com>
2017-08-17 13:49:19 +0200

    fix binding data to bmc.docx

--- middlewares/lti.js
+++ middlewares/lti.js
@@ -215,7 +215,11 @@ const serveDeliverableAsWord = (req, res) => {
     res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
     res.setHeader('Content-Disposition', `attachment; filename=${type}-${email}.docx`);
 
-    res.send(wordExport.render(deliverable, 'bmc.docx'));
+    if (type === 'businessmodelcanvas') {
+      res.send(wordExport.render(deliverable, 'bmc.docx'));
+    } else {
+      res.send(wordExport.render(deliverable));
+    }
   })
   .catch(err => {
     res.send(err);
