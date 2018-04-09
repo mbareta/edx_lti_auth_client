a7ef49a3215d207dc5b10dc42e0eb67dfe8f65c2
mbareta <mbareta@extensionengine.com>
2018-04-09 12:26:02 +0200

    KF-703: BMC - change modal text (#160)

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -37,6 +37,10 @@ const getUserProfile = (req, res, next) => {
 
 const cacheUserXBlocks = (req, res) => {
   // cache all user blocks in session
+  if (process.env.NODE_ENV !== 'production') {
+    return res.redirect(req.session.redirectToUrl || '/');
+  }
+
   edxCourseApi.getAllUserBlocks(req)
   .then(blocks => { req.session.blocks = blocks; })
   .then(() => res.redirect(req.session.redirectToUrl || '/'));
